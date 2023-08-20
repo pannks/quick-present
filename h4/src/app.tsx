fd7d77ec423c550cb54971aa31f60a1704ae613b
Pann Kaansadich <pannrattanapong@gmail.com>
2023-08-21 01:38:22 +0700

    init qwik

---
+++ h4/src/app.tsx
@@ -0,0 +1,11 @@
+import { component$ } from "@builder.io/qwik";
+
+import "./app.css";
+
+export const App = component$(() => {
+    return (
+        <>
+            <h1>Hello Qwik</h1>
+        </>
+    );
+});
